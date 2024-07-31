bee2c7823fd2458a201f46726b59728e4672ca23
ZxStim <thang.victor.luong@gmail.com>
2024-08-01 02:14:19 +0700

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,33 @@ import MobileWarning from '@/components/mobile-warning';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "BlockCMD Inspect",
-  description: "Interactive frontend for onchain contracts",
-};
+  title: 'BlockCMD Inspect',
+  description: 'Interact with any contracts on any chains',
+  metadataBase: new URL('https://www.blockcmd.com'),
+  openGraph: {
+    title: 'BlockCMD Inspect',
+    description: 'Interact with any contracts on any chains',
+    url: 'https://www.blockcmd.com',
+    siteName: 'BlockCMD',
+    images: [
+      {
+        url: '/blockcmd-tbn.png',
+        width: 1200,
+        height: 630,
+        alt: 'og-image',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'BlockCMD Inspect',
+    description: 'Interact with any contracts on any chains',
+    creator: '@zxstim',
+    images: ['/blockcmd-tbn.png'],
+  },
+}
 
 export default function RootLayout({
   children,
