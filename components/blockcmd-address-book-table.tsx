cfd73649b127fd8d9bddb6df219ed2890ecbab93
ZxStim <thang.victor.luong@gmail.com>
2024-08-01 00:14:15 +0700

    edit address book

--- components/blockcmd-address-book-table.tsx
+++ components/blockcmd-address-book-table.tsx
@@ -1,24 +1,24 @@
-"use client"
+"use client";
 import { DataTable } from "@/components/ui/data-table";
-import { ColumnDef } from "@tanstack/react-table"
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
+import { ColumnDef } from "@tanstack/react-table";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
- 
+
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 type AddressBookEntry = {
-  id: number
-  name: string
-  symbol: string
-  network: string
-  address: string
-}
- 
+  id: number;
+  name: string;
+  symbol: string;
+  network: string;
+  address: string;
+};
+
 const columns: ColumnDef<AddressBookEntry>[] = [
   {
     accessorKey: "id",
-    header: "Name",
+    header: "ID",
   },
   {
     accessorKey: "name",
@@ -35,23 +35,41 @@ const columns: ColumnDef<AddressBookEntry>[] = [
   {
     accessorKey: "address",
     header: "Address",
+    cell: ({ row }) => {
+      const entry = row.original;
+      return (
+        <a
+          className="underline underline-offset-2 text-blue-500"
+          target="_blank"
+          href={`https://klaytnfinder.io/address/${entry.address}`}
+        >
+          {entry.address}
+        </a>
+      );
+    },
   },
   {
     id: "actions",
     cell: ({ row }) => {
-      const entry = row.original
-
       return (
         <Button asChild>
-          <Link href={`/${row.getValue("network")}/address/${row.getValue("address")}`}>Go <ArrowRight className="ml-2 w-4 h-4" /></Link>
+          <Link
+            href={`/${row.getValue("network")}/address/${row.getValue(
+              "address"
+            )}`}
+          >
+            Go <ArrowRight className="ml-2 w-4 h-4" />
+          </Link>
         </Button>
-      )
-    }
+      );
+    },
   },
-]
+];
 
-export default function BlockcmdAddressBookTable( { blockcmdAddressBook }: { blockcmdAddressBook: AddressBookEntry[] }) {
-  return (
-    <DataTable columns={columns} data={blockcmdAddressBook} />
-  );
+export default function BlockcmdAddressBookTable({
+  blockcmdAddressBook,
+}: {
+  blockcmdAddressBook: AddressBookEntry[];
+}) {
+  return <DataTable columns={columns} data={blockcmdAddressBook} />;
 }
