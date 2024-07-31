2bdf2f0dfcd820b2da89cb3ffd7237e59f04d386
ZxStim <thang.victor.luong@gmail.com>
2024-08-01 00:37:18 +0700

    improved home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
 import { Chains } from "./data";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -8,6 +9,22 @@ export default function Home() {
       <div className="flex flex-col gap-4 text-center">
         <h1>BlockCMD Inspect</h1>
         <p>Interact with any contracts on any chains</p>
+      </div>
+      <div className="flex flex-col gap-4 text-left justify-start w-[400px]">
+        <Button asChild className="w-full">
+        <Link
+            href="/my-address-book"
+          >
+            My address book <ArrowRight className="ml-2 w-4 h-4" />
+          </Link>
+        </Button>
+        <Button asChild className="w-full">
+        <Link
+            href="/blockcmd-address-book"
+          >
+            BlockCMD address book <ArrowRight className="ml-2 w-4 h-4" />
+          </Link>
+        </Button>
       </div>
       <div className="flex flex-col gap-4 w-[400px]">
         {Chains.map((chain) => (
