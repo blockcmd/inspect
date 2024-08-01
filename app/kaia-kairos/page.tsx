0b2ab8c8905190883d4764d535d0b38b98f2790f
ZxStim <thang.victor.luong@gmail.com>
2024-08-01 11:04:36 +0700

    moved dashboard

---
+++ app/kaia-kairos/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { getAddress } from "viem";
+import { ArrowRight } from "lucide-react";
+import BlockcmdAddressBookTable from "@/components/blockcmd-address-book-table";
+import { blockcmdAddressBook, AddressBookEntry } from "@/components/blockcmd-address-book";
+
+export default function Page() {
+  const [contractAddress, setContractAddress] = useState("");
+
+  function filterAddressBookForKaia(blockcmdAddressBook: AddressBookEntry[]) {
+    return blockcmdAddressBook.filter((entry) => entry.network === "kaia-kairos");
+  }
+
+  function handleInputContractAddressChange(
+    e: React.ChangeEvent<HTMLInputElement>
+  ) {
+    setContractAddress(e.target.value);
+  }
+
+  return (
+    <div className="flex flex-col gap-12">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Kaia
+      </h1>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Kaia</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div className="flex flex-col gap-4">
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight">
+          Enter contract address
+        </h2>
+        <Input
+          className="border-2 border-primary rounded-none mt-4"
+          placeholder="0x..."
+          value={contractAddress}
+          onChange={handleInputContractAddressChange}
+        />
+        <Button asChild className="w-fit">
+          <Link
+            href={`/kaia-kairos/address/${
+              contractAddress ? getAddress(contractAddress) : ""
+            }`}
+          >
+            Go <ArrowRight className="ml-2 w-4 h-4" />
+          </Link>
+        </Button>
+      </div>
+      <p>or</p>
+      <div className="flex flex-col gap-4">
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight">
+          Select from your address book
+        </h2>
+        <Button variant="secondary" asChild className="w-fit hover:outline hover:outline-offset-1 hover:outline-2">
+          <Link
+            href="/my-address-book"
+          >
+            Manage my address book <ArrowRight className="ml-2 w-4 h-4" />
+          </Link>
+        </Button>
+      </div>
+      <p>or</p>
+      <div className="flex flex-col gap-4">
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight">
+          Select from BlockCMD address book
+        </h2>
+        <Button variant="secondary" asChild className="w-fit hover:outline hover:outline-offset-1 hover:outline-2">
+          <Link
+            href="/blockcmd-address-book"
+          >
+            See full BlockCMD address book <ArrowRight className="ml-2 w-4 h-4" />
+          </Link>
+        </Button>
+        <BlockcmdAddressBookTable blockcmdAddressBook={filterAddressBookForKaia(blockcmdAddressBook)} />
+      </div>
+    </div>
+  );
+}
